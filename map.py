5485ebb9ceb30a4a1246f11d5bc942fe5afc5ad0
WM <wmilczarek@gmail.com>
2022-04-15 14:09:55 +0200

    added another place. Sound description can now be stopped and resumed.

--- map.py
+++ map.py
@@ -4,7 +4,16 @@ from kivymd.uix.label import MDLabel
 from kivy.lang.builder import Builder   
 from kivy.core.window import Window
 
-from playsound import playsound
+from pygame import mixer
+
+descs = {'ratusz':{'img1':'Widok na ratusz od strony plantów',
+'img2':'Widok z lewej storny',
+'img3':'Widok od strony parku',
+'desc':'Ratusz we Wschowie – najstarsza częścią ratusza jest monumentalna wieża pochodząca z XVI wieku. W latach 1860 budowli nadano obecny charakter neoromański. Jest to budynek murowany, trójkondygnacyjny, założony na rzucie prostokąta. W narożniku północno-zachodnim znajduje się wieża. Zachowała się na parterze w części północno-wschodniej sala nakryta późnogotyckim sklepieniem sieciowym (I połowa XV wieku).'},
+'fara':{'img1':'Pierwsze zdjecie',
+'img2':'Drugie zdjecie',
+'img3':'Trzecie zdjecie',
+'desc':'Pierwsza wzmianka o istnieniu parafii we Wschowie pochodzi z dokumentu z 1326 roku, w którym miejscowy proboszcz Jordan pełnił rolę świadka. Sama parafia i kościół zapewne istniały już wcześniej. Budowla wielokrotnie została zniszczona podczas licznych pożarów Wschowy, między innymi w latach 1435 i 1529. Po drugim pożarze kościół został odbudowany, dzięki szczodrości i działaniom kanonika wrocławskiego Mateusza Lamprechta, urodzonego we Wschowie. Podczas reformacji, przez kilka lat, świątynia była używana przez protestantów. Zwrócili ją gminie katolickiej w 1604 roku. Po raz kolejny kościół obrócił się w zgliszcza w 1685 roku, a mała parafia katolicka nie mogła sobie poradzić z jego odbudowaniem. Ostatecznie, świątynia została odbudowana w latach 1720-1726 według projektu słynnego włoskiego architekta Pompeo Ferrariego. W takim stanie kościół zachował się do dnia dzisiejszego. Jest to kościół bazylikowy o trzech nawach, reprezentujący styl barokowy, z elementami architektury gotyckiej. Wyposażenie wnętrza jest utrzymane w stylu barokowym i pochodzi z XVIII stulecia. '}}
 
 
 class MapViewApp(MDApp):
@@ -24,11 +33,11 @@ class MapViewApp(MDApp):
         lon_diff = abs(self.root.ids.user.lon - self.root.ids.ratusz.lon)
         if lat_diff < 0.0003:
             if lon_diff < 0.0006:
-                self.root.ids.user.source = "user_ok.png"
+                self.root.ids.user.source = "data/img/user_ok.png"
             else:
-                self.root.ids.user.source = "user.png"
+                self.root.ids.user.source = "data/img/user.png"
         else:
-                self.root.ids.user.source = "user.png"
+                self.root.ids.user.source = "data/img/user.png"
         print(lon_diff) 
         print('after')
     
@@ -38,12 +47,43 @@ class MapViewApp(MDApp):
     def audiofile_setup(self,name):
         self.audiofile = name
         self.root.ids.play_sound.disabled = False
+        self.root.ids.pause_sound.disabled = False
+        self.root.ids.unpause_sound.disabled = False
     
     def play_sound(self):
-        playsound(f'{self.audiofile}.mp3')
+        mixer.init()
+        mixer.music.load(f"data/audio/{self.audiofile}.mp3")
+        mixer.music.play()
+    
+    def stop_sound(self):
+        try:
+            mixer.music.pause()
+        except:
+            pass
+    def unpause_sound(self):
+        try:
+            mixer.music.unpause()
+        except:
+            pass
 
     def disable_sound(self):
         self.root.ids.play_sound.disabled = True
+        self.root.ids.pause_sound.disabled = True
+        self.root.ids.unpause_sound.disabled = True
+
+    def enable_content(self,name):
+        #Setup images
+        self.root.ids.img1.background_normal = f"data/img/{name}_1.jpg"
+        self.root.ids.img1.background_down = f"data/img/{name}_1.jpg"
+        #Change later to jpg
+        self.root.ids.img2.source = f"data/img/{name}_2.jpg"
+        self.root.ids.img3.source = f"data/img/{name}_3.jpg"
+        #Setup img captions
+        self.root.ids.img1_caption.text = descs[name]['img1']
+        self.root.ids.img2_caption.text = descs[name]['img2']
+        self.root.ids.img3_caption.text = descs[name]['img3']
+        self.root.ids.item_desc.text = descs[name]['desc']
+        
 
 
 MapViewApp().run()
